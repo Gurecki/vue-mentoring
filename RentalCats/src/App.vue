7c17e4a34e203894b2bba7a996e4c8cbdb2777d2
Pietrzyk Marcin (FIAIB) <abj75@eur.corp.vattenfall.com>
2022-02-16 14:45:57 +0100

    Boilerplate

---
+++ RentalCats/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import MainView from '@/components/MainView.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <MainView :name="'Marcin'"/>
+    </div>
+  </header>
+
+</template>
+
+<style></style>
---
+++ RentalCats/src/components/MainView.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  // type inference enabled
+  props: {
+    name: String
+  },
+  data() {
+    return {
+      count: 1
+    }
+  },
+  mounted() {
+    console.log(this.name)
+  }
+
+})
+
+</script>
+
+<template>
+  <div class="main-vue">
+    <p>Hi {{ name }}! WELCOME TO CAT RENTAL</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
